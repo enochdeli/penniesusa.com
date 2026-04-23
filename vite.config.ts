0bd9f46470364d876402efccbe99a301217a8680
enochdeli <eenochdeli@gmail.com>
2026-04-22 23:02:40 -0400

    feat: Configure Vite for production
    Removes fast refresh configuration from Vite's React plugin and a redundant script tag from index.html. This ensures a clean and optimized build for production environments.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    fastRefresh: false
-  })],
+  plugins: [react()],
   server: {
     port: 3000,
     host: true,
